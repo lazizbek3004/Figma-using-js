f686cd032d475b92699cb6dcbfa95e289adad1d4
lazizbek3004 <lazizbekturayev194@gmail.com>
2024-02-15 11:53:30 +0500

    another good figma by using a bit of js codes

---
+++ js.js
@@ -0,0 +1,13 @@
+myNum = document.getElementById("initialValue");
+let count = 1;
+
+function decrease(){
+   count--;
+   initialValue.textContent = count;
+    
+}
+function increase(){
+    count++;
+    initialValue.textContent = count;
+}
+
